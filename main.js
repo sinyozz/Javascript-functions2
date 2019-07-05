e1ed5e43fd5e515ad51bc33f9634b309bc493126
Siny <51217784+sinyozz@users.noreply.github.com>
2019-07-05 00:34:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+//answer 1
+// 
+
+   
+document.querySelector
+const avg = items.reduce(function (a, b) {
+    return a + b.price
+}, 0) / items.length
+console.log(item)
+document.querySelector("#answer1").innerHTML=answer1
+const answer1 = `the average price is $$$`(avg.toFixed(23))
+
+const feedback = document.getElementById("feedback");
+feedback.innerHTML
+
+//answer2//
+
+const itemsRange = []
+items.forEach(function(item) {
+  if(item.price >= 14 && item.price <= 18)
+    itemsRange.push(item);
+});
+console.log("Items that cost between $14.00 USD and $18.00 USD:", itemsRange);
+
+//answer3//
+
+const GBP = items.reduce(function(name, price) { 
+return name+price(GBP,items);
+});
+console.log(GBP,name,price);
+
+
+
+//answer4//
+const woodItems = items.filter(function(item) {
+    return item.materials.indexOf("wood") >= 0;
+  });
+  
+  console.log(woodItems)
+
+//anwer5//
+ const Madeofeightormorematerials = items.map(function(items){
+         return Madeofeightormorematerials.items;
+
+ });
+
+
+
+//anwer6//
+conts.madebyseller = items.filter(function(items){
+    return madebyseller.byseller === 'items';
+
+});
+console.log(onlyitemsmadebyseller);
+
+
+
